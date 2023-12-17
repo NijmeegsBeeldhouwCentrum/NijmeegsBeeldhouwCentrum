79fc77b0b852b27ba70e523bfc1d17ab3ffc2c0d
Herre Groen <herregroen@gmail.com>
2023-12-17 15:25:02 +0100

    Make description bold

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -1,22 +1,25 @@
 import React from "react";
 import format from "date-fns/format";
+import Jumbotron from "./components/jumbotron";
+import { nl } from "date-fns/locale";
 
 export default class PostPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     const image = getAsset(entry.getIn(["data", "image"]));
 
-    return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
-      <div className="flex justify-between grey-3">
-        <p>{ format(entry.getIn(["data", "date"]), "iii, MMM d, yyyy") }</p>
-        <p>Read in x minutes</p>
+    return <>
+      <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
+      <div className="mw6 center ph3 pv4">
+        <div className="flex justify-between grey-3">
+          <p>{ format(entry.getIn(["data", "date"]), "iiii, d MMMM, yyyy", { locale: nl }) }</p>
+          <p>Lees in x minuten</p>
+        </div>
+        <div className="cms mw6">
+          <p class="b">{ entry.getIn(["data", "description"]) }</p>
+          { widgetFor("body") }
+        </div>
       </div>
-      <div className="cms mw6">
-        <p>{ entry.getIn(["data", "description"]) }</p>
-        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
-        { widgetFor("body") }
-      </div>
-    </div>;
+    </> ;
   }
 }
